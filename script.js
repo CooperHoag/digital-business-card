6aec2e270b5ce4be53ca9c6f618945237423a6ca
CooperHoag <cooperhoag@gmail.com>
2025-05-11 11:51:43 -0700

    Create initial digital business card

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("linkedinBtn");
+
+button.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/cooper-hoag-41b109191", "_blank");
+});
